36f89d84d8ee2fdcdb106680189488e59f518866
Nicole F <nicole.freed@gmail.com>
2019-05-19 14:24:33 -0700

    plan for animations

---
+++ index.js
@@ -0,0 +1,5 @@
+// dynamically load set of image urls (data) into cards
+// change width, height, and number of cards displayed based on data (there should be a max #)
+// close up spaces between cards based on position
+// hovering over a card permanently (?) shifts that card to the center, and all other cards to the -direction of the hovered card (e.g. if card is on the left, it shifts the whole carousel right)
+// clicking on a card (a second time?) goes to the page with that portfolio item (or flips card, in full npm version)
